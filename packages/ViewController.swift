eeb8e145fd31a9548531fa980646c844d6348153
thomas <degry.thomas@gmail.com>
2015-08-18 16:34:21 -0400

    Removed unnecessiry stuff

--- packages/ViewController.swift
+++ packages/ViewController.swift
@@ -12,16 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        println("Thomas sucks")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
